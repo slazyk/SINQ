a431d6b8971add59ddb92c8829ffcefc7d9bbb93
Leszek Ślażyński <slazyk@gmail.com>
2014-06-25 00:58:04 +0200

    Add Xcode project

---
+++ SINQ/SINQ.h
@@ -0,0 +1,19 @@
+//
+//  SINQ.h
+//  SINQ
+//
+//  Created by Leszek Ślażyński on 25/06/14.
+//  Copyright (c) 2014 Leszek Ślażyński. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SINQ.
+FOUNDATION_EXPORT double SINQVersionNumber;
+
+//! Project version string for SINQ.
+FOUNDATION_EXPORT const unsigned char SINQVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SINQ/PublicHeader.h>
+
+
---
+++ SINQ/SINQ.swift
@@ -0,0 +1,8 @@
+//
+//  SINQ.swift
+//  SINQ
+//
+//  Created by Leszek Ślażyński on 25/06/14.
+//  Copyright (c) 2014 Leszek Ślażyński. All rights reserved.
+//
+
